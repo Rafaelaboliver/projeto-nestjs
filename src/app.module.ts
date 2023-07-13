4c0b07cc9cd7c6bae3968daceffa18f1b8ab9de0
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-07-13 16:55:17 -0300

    feat: prisma connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { PublicationModule } from './modules/publication/publication.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [UserModule, PublicationModule],
+  imports: [UserModule, PublicationModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 6,
+    minLowercase: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()  
+  avatar:string;
+}
